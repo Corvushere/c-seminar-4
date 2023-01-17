f6049e48262e12bd6b496ddbc99b310bd816f18d
Corvushere <corvushere@gmail.com>
2023-01-17 23:58:19 +0300

    init

--- program.cs
+++ program.cs
@@ -1,10 +1,11 @@
 ﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
 
 
-nt a, b, sqгare;
+int a, b;
 Console.WriteLine("Введите число 'A'");
 a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 'B'");
 b = Convert.ToInt32(Console.ReadLine());
-    double sqгare = Math.Pow(a, b);
-    Console.Write("Если возведём число 'А' в степень 'А', получим: " + square);
+    void square {
+    double square = Math.Pow(a, b);}
+Console.Write("Если возведём число 'А' в степень 'B', получим: " + square);
