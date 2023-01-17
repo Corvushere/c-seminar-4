96f23c4ea8001e9b86511b535c8073d08038430c
Corvushere <corvushere@gmail.com>
2023-01-17 23:55:15 +0300

    init

--- program.cs
+++ program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+
+nt a, b, sqгare;
+Console.WriteLine("Введите число 'A'");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 'B'");
+b = Convert.ToInt32(Console.ReadLine());
+    double sqгare = Math.Pow(a, b);
+    Console.Write("Если возведём число 'А' в степень 'А', получим: " + square);
